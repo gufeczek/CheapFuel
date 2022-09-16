e3ec0858ee4f3486b6d4b5044cb7534d8044b511
gufeczek <gufeczek@gmail.com>
2022-09-16 18:20:48 +0200

    Added gradient to welcome message TextView in LoginActivity

--- app/src/main/java/com/example/fuel/LoginActivity.kt
+++ app/src/main/java/com/example/fuel/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.example.fuel
 
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var tvWelcomeMsg: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        tvWelcomeMsg = findViewById(R.id.loginActivity_welcome)
+        /*TODO: change gradient values*/
+        val shader = LinearGradient(0f, 0f, 0f, tvWelcomeMsg.textSize,
+            Color.RED, Color.BLUE, Shader.TileMode.CLAMP)
+        tvWelcomeMsg.paint.shader = shader
     }
 }
