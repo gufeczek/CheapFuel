452e7c90f25c2ab31acf9e7a1dba3c852a0da97a
Fenrir7734 <hitex1999@gmail.com>
2022-10-13 21:13:35 +0200

    Changed db provider from MySql.Data.EntityFrameworkCore to Pomelo.EntityFrameworkCore.MySql

--- services/src/Infrastructure/ConfigureServices.cs
+++ services/src/Infrastructure/ConfigureServices.cs
@@ -3,6 +3,7 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure;
 
@@ -17,9 +18,15 @@ public static class ConfigureServices
         }
         else
         {
-            services.AddDbContext<AppDbContext>(c =>
-                c.UseMySQL(configuration.GetConnectionString("DatabaseConnection") 
-                           ?? throw new AppConfigurationException("Database connection string is missing")));
+            var connectionString = configuration.GetConnectionString("DatabaseConnection")
+                                   ?? throw new AppConfigurationException("Database connection string is missing");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 0));
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseMySql(connectionString, serverVersion)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors());
         }
 
         return services;
