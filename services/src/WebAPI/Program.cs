0b877baa14e53f8f3c6b9f7f43969d92c054ef0b
Fenrir7734 <hitex1999@gmail.com>
2022-10-13 19:42:48 +0200

    Added references between project, added entity framework and connection to db

--- services/src/WebAPI/Program.cs
+++ services/src/WebAPI/Program.cs
@@ -1,9 +1,11 @@
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
