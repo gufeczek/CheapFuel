bfb4e168eaa27f0072e991231254299393557346
Fenrir7734 <hitex1999@gmail.com>
2022-10-17 18:50:54 +0200

    removed public access modifier from interface

--- services/src/Domain/Interfaces/IUnitOfWork.cs
+++ services/src/Domain/Interfaces/IUnitOfWork.cs
@@ -4,18 +4,18 @@ namespace Domain.Interfaces;
 
 public interface IUnitOfWork : IDisposable
 {
-    public IFavoriteRepository Favorites { get; }
-    public IFuelAtStationRepository FuelsAtStation { get; }
-    public IFuelPriceRepository FuelPrices { get; }
-    public IFuelStationRepository FuelStations { get; }
-    public IFuelTypeRepository FuelTypes { get; }
-    public IOpeningClosingTimeRepository OpeningClosingTimes { get; }
-    public IOwnedStationRepository OwnedStations { get; }
-    public IReviewRepository Reviews { get; }
-    public IServiceAtStationRepository ServicesAtStation { get; }
-    public IServiceRepository Services { get; }
-    public IStationChainRepository StationChains { get; }
-    public IUserRepository Users { get; }
+    IFavoriteRepository Favorites { get; }
+    IFuelAtStationRepository FuelsAtStation { get; }
+    IFuelPriceRepository FuelPrices { get; }
+    IFuelStationRepository FuelStations { get; }
+    IFuelTypeRepository FuelTypes { get; }
+    IOpeningClosingTimeRepository OpeningClosingTimes { get; }
+    IOwnedStationRepository OwnedStations { get; }
+    IReviewRepository Reviews { get; }
+    IServiceAtStationRepository ServicesAtStation { get; }
+    IServiceRepository Services { get; }
+    IStationChainRepository StationChains { get; }
+    IUserRepository Users { get; }
 
     Task SaveAsync();
 }
