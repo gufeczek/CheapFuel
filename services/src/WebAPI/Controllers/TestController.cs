465a5eec69b4cf5dbeec1434c184e98022c913ff
Fenrir7734 <hitex1999@gmail.com>
2022-10-22 11:19:22 +0200

    Added test controller

---
+++ services/src/WebAPI/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Common.Authorization;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [Authorize]
+    [HttpGet("user")]
+    public string GetUser()
+    {
+        return "Hello world user!";
+    }
+
+    [AuthorizeAdmin]
+    [HttpGet("admin")]
+    public string GetAdmin()
+    {
+        return "Hello world admin!";
+    }
+
+    [AuthorizeOwner]
+    [HttpGet("Owner")]
+    public string GetOwner()
+    {
+        return "Hello world owner!";
+    }
+}
