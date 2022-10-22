ee167597c678a9dc5dc09ab8d561a646ea6e6a42
Fenrir7734 <hitex1999@gmail.com>
2022-10-22 18:33:24 +0200

    Swagger annotations and authorization

---
+++ services/src/WebAPI/ConfigureServices.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+using WebAPI.Common.Authorization.Swagger;
+
+namespace WebAPI;
+
+public static class ConfigureServices
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.EnableAnnotations();
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Cheap Fuel API", Version = "v1" });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                In = ParameterLocation.Header,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                Name = "Authorization",
+                Description = "JWT Authorization header using the Bearer scheme."
+            });
+            options.OperationFilter<AuthorizationOperationFilter>();
+        });
+    }
+}
