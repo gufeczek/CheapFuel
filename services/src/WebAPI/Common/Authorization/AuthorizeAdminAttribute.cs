e21c5e0da0271efedea15909bacb1210bd328e8b
Fenrir7734 <hitex1999@gmail.com>
2022-10-22 11:18:34 +0200

    Authorize attributes

---
+++ services/src/WebAPI/Common/Authorization/AuthorizeAdminAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebAPI.Common.Authorization;
+
+public class AuthorizeAdminAttribute : AuthorizeAttribute
+{
+    public AuthorizeAdminAttribute()
+    {
+        Roles = Role.Admin.ToString();
+    }
+}
