5506a684e4cf83611f1dcfdf466c61de1637d18a
Fenrir7734 <hitex1999@gmail.com>
2022-10-25 19:51:29 +0200

    Separate controllers to command and query

--- services/src/WebAPI/Controllers/FuelStationService/FuelStationServiceCommandController.cs
+++ services/src/WebAPI/Controllers/FuelStationService/FuelStationServiceCommandController.cs
@@ -1,33 +1,24 @@
 ﻿using Application.FuelStationServices.Commands.CreateFuelStationService;
 using Application.FuelStationServices.Commands.DeleteService;
-using Application.FuelStationServices.Queries.GetAllFuelStationServices;
 using Application.Models;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.Common.Authorization;
 
-namespace WebAPI.Controllers;
+namespace WebAPI.Controllers.FuelStationService;
 
 [ApiController]
+[AuthorizeAdmin]
 [Route("api/v1/services")]
-public class FuelStationServicesController : ControllerBase
+public class FuelStationServiceCommandController : ControllerBase
 {
     private readonly IMediator _mediator;
 
-    public FuelStationServicesController(IMediator mediator)
+    public FuelStationServiceCommandController(IMediator mediator)
     {
         _mediator = mediator;
     }
-
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<FuelStationServiceDto>>> GetAllAsync()
-    {
-        var result = await _mediator.Send(new GetAllFuelStationServicesQuery());
-        return Ok(result);
-    }
-
-    [AuthorizeAdmin]
+    
     [HttpPost]
     public async Task<ActionResult<FuelStationServiceDto>> CreateService([FromBody] CreateFuelStationServiceCommand command)
     {
@@ -35,7 +26,6 @@ public class FuelStationServicesController : ControllerBase
         return Ok(result);
     }
 
-    [AuthorizeAdmin]
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteService([FromRoute] long id)
     {
