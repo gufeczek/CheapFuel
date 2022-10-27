dd3402dac4afa958663180a4dda9a70aa3e9cf51
gufeczek <gufeczek@gmail.com>
2022-10-27 18:37:45 +0200

    Refactored id names to conform Navigation framework

--- ui/app/src/main/java/com/example/fuel/ui/fragment/SetRegisterMethodFragment.kt
+++ ui/app/src/main/java/com/example/fuel/ui/fragment/SetRegisterMethodFragment.kt
@@ -5,9 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.fuel.R
+import com.example.fuel.databinding.FragmentSetRegisterMethodBinding
 
 class SetRegisterMethodFragment : Fragment(R.layout.fragment_set_register_method) {
+
+    private lateinit var binding: FragmentSetRegisterMethodBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSetRegisterMethodBinding.inflate(inflater, container, false)
+        binding.btnRegister.setOnClickListener{
+            Navigation.findNavController(binding.root).navigate(R.id.setUsernameFragment)
+        }
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
--- ui/app/src/main/java/com/example/fuel/ui/fragment/SetUsernameFragment.kt
+++ ui/app/src/main/java/com/example/fuel/ui/fragment/SetUsernameFragment.kt
@@ -8,10 +8,13 @@ import androidx.fragment.app.Fragment
 import com.example.fuel.R
 
 class SetUsernameFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_set_username, container, false)
+        val view = inflater.inflate(R.layout.fragment_set_username, container, false)
+
+        return view
     }
 }
