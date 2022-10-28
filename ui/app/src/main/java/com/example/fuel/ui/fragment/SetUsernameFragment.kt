a0e3d99a0ad2ca11e6298c47a7a14d9973eaeaad
gufeczek <gufeczek@gmail.com>
2022-10-29 00:42:30 +0200

    Split into methods

--- ui/app/src/main/java/com/example/fuel/ui/fragment/SetUsernameFragment.kt
+++ ui/app/src/main/java/com/example/fuel/ui/fragment/SetUsernameFragment.kt
@@ -4,10 +4,10 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.fuel.R
@@ -15,7 +15,7 @@ import com.example.fuel.databinding.FragmentSetUsernameBinding
 import com.example.fuel.ui.utils.extension.EditTextExtension.Companion.afterTextChanged
 import com.example.fuel.ui.utils.extension.TextViewExtension.Companion.removeLinksUnderline
 import java.util.*
-import kotlin.jvm.optionals.toSet
+
 
 class SetUsernameFragment : Fragment() {
 
@@ -36,35 +36,8 @@ class SetUsernameFragment : Fragment() {
         binding.tvTermsOfUse.movementMethod = LinkMovementMethod.getInstance()
         binding.tvTermsOfUse.removeLinksUnderline()
 
-        binding.btnNextPage.setOnClickListener {
-            val (isValidationPassed: Boolean, _msg: Optional<ValidationErrors>) = validateUsername(binding.etUsername.text.toString())
-            if (!isValidationPassed && _msg.isPresent) {
-                val msg: ValidationErrors = _msg.get()
-                binding.tilUsername.setBackgroundResource(R.drawable.bg_rounded_error)
-                binding.tvUsernameValidationError.text = msg.toString()
-                binding.tvUsernameValidationError.visibility = View.VISIBLE
-                if (msg == ValidationErrors.ERROR_USERNAME_TOO_SHORT || msg == ValidationErrors.ERROR_USERNAME_TOO_LONG) {
-                    binding.etUsername.afterTextChanged { editable ->
-                        if (validateUsername(editable).first) {
-                            binding.tilUsername.setBackgroundResource(R.drawable.bg_rounded)
-                            binding.tvUsernameValidationError.visibility = View.INVISIBLE
-                        }
-                    }
-                }
-            }
-            else if (!binding.chkTermsOfUse.isChecked) {
-                binding.tvTermsOfUseError.visibility = View.VISIBLE
-                binding.chkTermsOfUse.buttonTintList = ColorStateList.valueOf(Color.RED)
-                binding.tvTermsOfUse.setLinkTextColor(Color.RED)
-            }
-        }
-        binding.chkTermsOfUse.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.tvTermsOfUseError.visibility = View.GONE
-                binding.chkTermsOfUse.buttonTintList = ColorStateList.valueOf(resources.getColor(R.color.checkbox, activity?.theme))
-                binding.tvTermsOfUse.setLinkTextColor(resources.getColor(R.color.checkbox, activity?.theme))
-            }
-        }
+        binding.btnNextPage.setOnClickListener(btnValidationListener)
+        binding.chkTermsOfUse.setOnCheckedChangeListener(chkValidationListener)
 
         return binding.root
     }
@@ -76,5 +49,36 @@ class SetUsernameFragment : Fragment() {
             return Pair(false, Optional.of(ValidationErrors.ERROR_USERNAME_TOO_LONG))
         }
         return Pair(true, Optional.empty<ValidationErrors>())
+    }
+
+    private val btnValidationListener = View.OnClickListener {
+        val (isValidationPassed: Boolean, _msg: Optional<ValidationErrors>) = validateUsername(binding.etUsername.text.toString())
+        if (!isValidationPassed && _msg.isPresent) {
+            val msg: ValidationErrors = _msg.get()
+            binding.tilUsername.setBackgroundResource(R.drawable.bg_rounded_error)
+            binding.tvUsernameValidationError.text = msg.toString()
+            binding.tvUsernameValidationError.visibility = View.VISIBLE
+            if (msg == ValidationErrors.ERROR_USERNAME_TOO_SHORT || msg == ValidationErrors.ERROR_USERNAME_TOO_LONG) {
+                binding.etUsername.afterTextChanged { editable ->
+                    if (validateUsername(editable).first) {
+                        binding.tilUsername.setBackgroundResource(R.drawable.bg_rounded)
+                        binding.tvUsernameValidationError.visibility = View.INVISIBLE
+                    }
+                }
+            }
+        }
+        else if (!binding.chkTermsOfUse.isChecked) {
+            binding.tvTermsOfUseError.visibility = View.VISIBLE
+            binding.chkTermsOfUse.buttonTintList = ColorStateList.valueOf(Color.RED)
+            binding.tvTermsOfUse.setLinkTextColor(Color.RED)
+        }
+    }
+
+    private val chkValidationListener = CompoundButton.OnCheckedChangeListener { _, isChecked ->
+        if (isChecked) {
+            binding.tvTermsOfUseError.visibility = View.GONE
+            binding.chkTermsOfUse.buttonTintList = ColorStateList.valueOf(resources.getColor(R.color.checkbox, activity?.theme))
+            binding.tvTermsOfUse.setLinkTextColor(resources.getColor(R.color.checkbox, activity?.theme))
+        }
     }
 }
