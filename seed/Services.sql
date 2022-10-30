b5d0cfa4974fc6a0306cc7ac5154af8030f12ded
michalek990 <93988101+michalek990@users.noreply.github.com>
2022-10-30 16:39:07 +0100

    DataBase-seeder

---
+++ seed/Services.sql
@@ -0,0 +1,8 @@
+INSERT INTO CheapFuelDB.Services (Id, Name, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy) VALUES (14, 'Refueling', '2022-10-30 15:37:06.023898', 1, '2022-10-30 15:37:06.023960', 1);
+INSERT INTO CheapFuelDB.Services (Id, Name, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy) VALUES (15, 'Toilets', '2022-10-30 15:37:11.278961', 1, '2022-10-30 15:37:11.279023', 1);
+INSERT INTO CheapFuelDB.Services (Id, Name, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy) VALUES (16, 'Shower', '2022-10-30 15:37:17.391802', 1, '2022-10-30 15:37:17.391872', 1);
+INSERT INTO CheapFuelDB.Services (Id, Name, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy) VALUES (17, 'Resteurant', '2022-10-30 15:37:21.562750', 1, '2022-10-30 15:37:21.562811', 1);
+INSERT INTO CheapFuelDB.Services (Id, Name, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy) VALUES (18, 'Coffee', '2022-10-30 15:37:25.183652', 1, '2022-10-30 15:37:25.183720', 1);
+INSERT INTO CheapFuelDB.Services (Id, Name, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy) VALUES (19, 'Place to sleep', '2022-10-30 15:37:32.718257', 1, '2022-10-30 15:37:32.718317', 1);
+INSERT INTO CheapFuelDB.Services (Id, Name, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy) VALUES (20, 'Phone', '2022-10-30 15:37:38.600998', 1, '2022-10-30 15:37:38.601065', 1);
+INSERT INTO CheapFuelDB.Services (Id, Name, CreatedAt, CreatedBy, UpdatedAt, UpdatedBy) VALUES (21, 'ATM', '2022-10-30 15:37:47.982160', 1, '2022-10-30 15:37:47.982225', 1);
